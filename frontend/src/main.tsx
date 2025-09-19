3262e6f2f6219e7a74abd448229fed7c41b5a0dc
18barbers2 <sambarber101@gmail.com>
2025-09-18 23:48:47 -0400

    refactor: remove app component from main page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import CardBuilder from './pages/CardBuilder.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     <CardBuilder />
   </StrictMode>,
 )

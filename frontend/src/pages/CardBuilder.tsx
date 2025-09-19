e82e8453c8151476e4b826b7257466784ae6513e
18barbers2 <sambarber101@gmail.com>
2025-09-19 00:01:33 -0400

    feature: added temporary navbar for testing

--- frontend/src/pages/CardBuilder.tsx
+++ frontend/src/pages/CardBuilder.tsx
@@ -1,7 +1,23 @@
-export default function CardBuilder(){
+
+function NavBarTest () {
+    return (
+        <>
+            <div style={{backgroundColor: '#303236', width: '5%', height: '100%', display: 'flex', flexDirection: 'column', position: 'fixed', justifyContent: 'center', rowGap: '100px', alignItems: 'center'}}>
+                <a href="#" target="_blank">Home</a>
+                <a href="#" target="_blank">Card Builder</a>
+                <a href="#" target="_blank">Share</a>
+                <a href="#" target="_blank">etc</a>
+            </div>
+        </>
+    );
+}
+
+
+export default function CardBuilder() {
     return (
         <div>
-            <h1>Card Builder</h1>
+            {/* <h1>Card Builder</h1> */}
+            <NavBarTest></NavBarTest>
         </div>
     );
 }

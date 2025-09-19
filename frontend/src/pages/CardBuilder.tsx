becd4330c8c43ce700e61b255dacf0e2ce85717c
18barbers2 <sambarber101@gmail.com>
2025-09-19 00:20:49 -0400

    style: added link to font-awesome and updated navbar links

--- frontend/src/pages/CardBuilder.tsx
+++ frontend/src/pages/CardBuilder.tsx
@@ -1,14 +1,18 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+
 const FontStyle = {color: 'white', textDecoration: 'none'}
 
 
 function NavBarTest () {
+
     return (
         <>
-            <div style={{backgroundColor: '#303236', width: '5%', height: '100%', display: 'flex', flexDirection: 'column', position: 'fixed', justifyContent: 'center', rowGap: '100px', alignItems: 'center'}}>
-                <a href="#" target="_blank" style={FontStyle}>Home</a>
-                <a href="#" target="_blank" style={FontStyle}>Card Builder</a>
-                <a href="#" target="_blank" style={FontStyle}>Share</a>
-                <a href="#" target="_blank" style={FontStyle}>etc</a>
+            <div style={{backgroundColor: '#303236', left: '0', top: '0', width: '250px', height: '100%', display: 'flex', flexDirection: 'column', position: 'fixed', justifyContent: 'center', rowGap: '100px', alignItems: 'center'}}>
+                <a href="#home" target="_blank" style={FontStyle} className="fa fa-fw fa-home">Home</a>
+                <a href="#builder" target="_blank" style={FontStyle}><i className="fa fa-fw fa-home"></i>Card Builder</a>
+                <a href="#share" target="_blank" style={FontStyle} className="fa fa-fw fa-home">Share</a>
+                <a href="#etc" target="_blank" style={FontStyle} className="fa fa-fw fa-home">etc</a>
             </div>
         </>
     );

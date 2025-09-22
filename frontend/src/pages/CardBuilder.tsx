5391925f55b9e8c6bf962b0d480d9559b9746bf8
18barbers2 <sambarber101@gmail.com>
2025-09-22 19:26:51 -0400

    fix: issue CardBuilder export

--- frontend/src/pages/CardBuilder.tsx
+++ frontend/src/pages/CardBuilder.tsx
@@ -1,9 +1,14 @@
+import '../css/CardBuilder.css';
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+
 
 export default function CardBuilder() {
     return (
         <div>
-            <h1>Card Builder</h1>
+            <h1 className='page-title'>Card Builder</h1>
+            
         </div>
     );
 }

fbcb1305d0556ff38dbadda8ed7054aa4f9317bf
18barbers2 <sambarber101@gmail.com>
2025-09-22 20:26:16 -0400

    feature: added some css for card preview

--- frontend/src/pages/CardBuilder.tsx
+++ frontend/src/pages/CardBuilder.tsx
@@ -3,22 +3,32 @@ import '../css/CardBuilder.css';
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
 
-
 function TextBox() {
     return (
         <div>
             <textarea className={'card-input'} rows={40} cols={100} defaultValue={'card text goes here'}/>
         </div>
-    )
+    );
 }
 
+function CardPreview() {
+    return(
+        <div className='card-preview'>
+            
+        </div>
+    );
+}
 export default function CardBuilder() {
     return (
         <div className='card-builder-page'>
             <h1 className='page-title'>Card Builder</h1>
             <div className='card-builder'>
                 <TextBox></TextBox>
+                <CardPreview>
+                    
+                </CardPreview>
             </div>
         </div>
     );
 }
+

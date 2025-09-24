da4b6b198786bc41a534883a40e15b32dd9202fb
18barbers2 <sambarber101@gmail.com>
2025-09-23 20:18:51 -0400

    feature: update card preview to get entire html

--- frontend/src/pages/CardBuilder.tsx
+++ frontend/src/pages/CardBuilder.tsx
@@ -1,20 +1,44 @@
 import { useState } from 'react';
 import '../css/CardBuilder.css';
 
+// Test data to inject to the iframe
+const TestCardDoc = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Card Document</title>
+    <style>
+        html, body { margin:0; padding:0; }
+        body {
+            font-size: 25px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Content of body</h1>
+    <p>Paragraph</p>
+</body>
+</html>`;
+
 // const testHtml = `<div class='card' style='background-color: white'>User's HTML goes here</div>`;
 const iFrameStyle = {backgroundColor: '#e5e5e5ff', border: '0', width: '50%', height: '75%', borderRadius: '10px'};
 
 
+// Text input for user
 function TextBox({ html, onChange } : {html : string; onChange : (v: string) => void}) {
     return (
         <textarea className={'card-input'} rows={40} cols={100} value={html} onChange={e => onChange(e.target.value)} />
     );
 }
 
+// Card preview for user
 function CardPreview({html} : { html: string }) {
     return(
         <div className='card-preview'>
-            <iframe title='Card Preview' sandbox="" style={iFrameStyle} srcDoc={html}>
+            <iframe title='Card Preview' sandbox="" style={iFrameStyle} srcDoc={TestCardDoc}>
             </iframe>
         </div>
     );

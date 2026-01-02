1062ac1d103e6a38756f1d52455f43e4f8bcdfba
18barbers2 <sambarber101@gmail.com>
2026-01-02 18:01:40 -0500

    feature: updated Card to take props of any type, set inner html to props.html

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,7 +1,14 @@
-export default function Card() {
+//TODO: add in an interface for CardProps
+
+function Card( props : any) {
+
+    const html = props.html;
 
     return( 
-        <div>Card</div>
+        <div className="card">
+            <div dangerouslySetInnerHTML={{ __html : html }}></div>
+        </div>
     );
-    
 }
+
+export default Card;

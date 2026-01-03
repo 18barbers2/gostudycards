93cfe95df78be3b5899357e983f4badc28fd60ba
18barbers2 <sambarber101@gmail.com>
2026-01-03 00:36:34 -0500

    feature: added functionality to parse the template string for any keys

--- frontend/src/utils/templateParser.tsx
+++ frontend/src/utils/templateParser.tsx
@@ -1,5 +1,9 @@
-function parseTemplate ( template : string, data : any) {
-    return (
-        
-    );
+export function parseTemplate ( template : string, data : any): string {
+    let result = template;
+
+    for (let key in data) {
+        result = result.replace(`{{${key}}}`, data[key]);
+    }
+
+    return result;
 }

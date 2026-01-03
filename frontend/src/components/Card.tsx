924ce3e0ba2bde2186d61e992b45d9786752a40f
18barbers2 <sambarber101@gmail.com>
2026-01-03 00:37:17 -0500

    feature: use parse functiontion to set the innerhtml

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,12 +1,16 @@
 //TODO: add in an interface for CardProps
 
+import { parseTemplate } from "../utils/templateParser";
+
+// TODO: replace props with the template and data props
 function Card( props : any) {
 
     const template = props.template;
+    const html = parseTemplate(props.template, props.data);
 
     return( 
         <div className="card">
-            <div dangerouslySetInnerHTML={{ __html : template }}></div>
+            <div dangerouslySetInnerHTML={{ __html : html }}></div>
         </div>
     );
 }

55eed20315635d857699e21e46407d4cc1c9b3df
18barbers2 <sambarber101@gmail.com>
2026-01-05 00:04:17 -0500

    feature: added Question, Answer and Hint fields

--- frontend/src/pages/AddCard.tsx
+++ frontend/src/pages/AddCard.tsx
@@ -1,7 +1,28 @@
 import '../css/AddCard.css';
 
+function FieldInput({ html, onChange, fieldName } : {html : string; onChange : (v: string) => void; fieldName : string}) {
+    return (
+        <div>
+            <h1>{fieldName}</h1>
+            <textarea className={fieldName} rows={5} cols={100} value={html} onChange={e => onChange(e.target.value) }/>
+        </div>
+    );
+}
+
+
 export default function AddCard () {
+
     return(
-        <div className="add-card-page">AddCard</div>
+        <div className="add-card-page">
+            <FieldInput html={''} onChange={function (v: string): void {
+                throw new Error('Function not implemented.');
+            } } fieldName={'Question'}></FieldInput>
+            <FieldInput html={''} onChange={function (v: string): void {
+                throw new Error('Function not implemented.');
+            } } fieldName={'Answer'}></FieldInput>
+            <FieldInput html={''} onChange={function (v: string): void {
+                throw new Error('Function not implemented.');
+            } } fieldName={'Hint'}></FieldInput>
+        </div>
     );
 }

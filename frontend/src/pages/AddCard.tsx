911a339b7d0a1ad62e17d552e940dfef933e5470
18barbers2 <sambarber101@gmail.com>
2026-01-06 00:36:13 -0500

    style: added field name class

--- frontend/src/pages/AddCard.tsx
+++ frontend/src/pages/AddCard.tsx
@@ -3,7 +3,7 @@ import '../css/AddCard.css';
 function FieldInput({ html, onChange, fieldName } : {html : string; onChange : (v: string) => void; fieldName : string}) {
     return (
         <div>
-            <h1>{fieldName}</h1>
+            <h1 className='field-name'>{fieldName}</h1>
             <textarea className={`field-input ${fieldName}`} rows={5} cols={100} value={html} onChange={e => onChange(e.target.value) }/>
         </div>
     );

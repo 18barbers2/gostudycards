1563971ceb0cd37b00a38b7ca23321648053efaf
18barbers2 <sambarber101@gmail.com>
2026-01-06 00:20:48 -0500

    refactor: grouped buttons for easier styling

--- frontend/src/pages/AddCard.tsx
+++ frontend/src/pages/AddCard.tsx
@@ -17,26 +17,31 @@ export default function AddCard () {
             <h1 className='page-title'>Add Card</h1>
             <br />
             <div className='editor-format-controls'>
-                <button className='button-left'><b>B</b></button>
-                <button className='button-middle'><i>I</i></button>
-                <button className='button-middle'><u>U</u></button>
-                <button className='button-right'><s>S</s></button>
-                
-                <button className='button-left'>x<sup>2</sup></button>
-                <button className='button-right'>x<sub>2</sub></button>
-                
-                <button className='button-left'><span className='material-symbols-outlined'>format_color_text</span></button>
-                <button className='button-middle'><span className='material-symbols-outlined'>ink_highlighter</span></button>
-                <button className='button-right'><span className='material-symbols-outlined'>format_clear</span></button>
-
-                <button className='button-left'><span className='material-symbols-outlined'>format_list_numbered</span></button>
-                <button className='button-middle'><span className='material-symbols-outlined'>format_list_bulleted</span></button>
-                <button className='button-right'><span className='material-symbols-outlined'>format_align_center</span></button>
-                
-                <button className='button-left'><span className='material-symbols-outlined'>image</span></button>
-                <button className='button-middle'><span className='material-symbols-outlined'>mic</span></button>
-                <button className='button-right'><span className='material-symbols-outlined'>function</span></button>                
-                
+                <div className='button-group'>
+                    <button className='button-left'><b>B</b></button>
+                    <button className='button-middle'><i>I</i></button>
+                    <button className='button-middle'><u>U</u></button>
+                    <button className='button-right'><s>S</s></button>
+                </div>
+                <div className='button-group'>
+                    <button className='button-left'>x<sup>2</sup></button>
+                    <button className='button-right'>x<sub>2</sub></button>
+                </div>
+                <div className='button-group'>
+                    <button className='button-left'><span className='material-symbols-outlined'>format_color_text</span></button>
+                    <button className='button-middle'><span className='material-symbols-outlined'>ink_highlighter</span></button>
+                    <button className='button-right'><span className='material-symbols-outlined'>format_clear</span></button>
+                </div>
+                <div className='button-group'>
+                    <button className='button-left'><span className='material-symbols-outlined'>format_list_numbered</span></button>
+                    <button className='button-middle'><span className='material-symbols-outlined'>format_list_bulleted</span></button>
+                    <button className='button-right'><span className='material-symbols-outlined'>format_align_center</span></button>
+                </div>
+                <div className='button-group'>
+                    <button className='button-left'><span className='material-symbols-outlined'>image</span></button>
+                    <button className='button-middle'><span className='material-symbols-outlined'>mic</span></button>
+                    <button className='button-right'><span className='material-symbols-outlined'>function</span></button>
+                </div>
             </div>
 
 

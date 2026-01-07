0c047557592caaa68068c913252879b49e1aeae4
18barbers2 <sambarber101@gmail.com>
2026-01-06 21:09:29 -0500

    style: updated style of format controls

--- frontend/src/pages/AddCard.tsx
+++ frontend/src/pages/AddCard.tsx
@@ -3,8 +3,9 @@ import '../css/AddCard.css';
 function FieldInput({ html, onChange, fieldName } : {html : string; onChange : (v: string) => void; fieldName : string}) {
     return (
         <div>
+            <span className='material-symbols-outlined'>arrow_drop_down</span>
             <h1 className='field-name'>{fieldName}</h1>
-            <textarea className={`field-input ${fieldName}`} rows={5} cols={100} value={html} onChange={e => onChange(e.target.value) }/>
+            <textarea className={`field-input ${fieldName}`} rows={5} cols={110} value={html} onChange={e => onChange(e.target.value) }/>
         </div>
     );
 }

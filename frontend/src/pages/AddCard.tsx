f1673ef2a5d214658b304023c23ab90a30ad8c41
18barbers2 <sambarber101@gmail.com>
2026-01-06 21:33:28 -0500

    fix: added missing div

--- frontend/src/pages/AddCard.tsx
+++ frontend/src/pages/AddCard.tsx
@@ -1,61 +1,61 @@
 import '../css/AddCard.css';
 
-function FieldInput({ html, onChange, fieldName } : {html : string; onChange : (v: string) => void; fieldName : string}) {
+function FieldInput({ html, onChange, fieldName }: { html: string; onChange: (v: string) => void; fieldName: string }) {
     return (
         <div>
             <span className='material-symbols-outlined'>arrow_drop_down</span>
             <h1 className='field-name'>{fieldName}</h1>
-            <textarea className={`field-input ${fieldName}`} rows={5} cols={110} value={html} onChange={e => onChange(e.target.value) }/>
+            <textarea className={`field-input ${fieldName}`} rows={5} cols={110} value={html} onChange={e => onChange(e.target.value)} />
         </div>
     );
 }
 
 
-export default function AddCard () {
+export default function AddCard() {
 
-    return(
+    return (
         <div className='add-card-page'>
             <h1 className='page-title'>Add Card</h1>
             <br />
             <div className='input-section'>
                 <div className='editor-format-controls'>
-                <div className='button-group'>
-                    <button className='button-left'><b>B</b></button>
-                    <button className='button-middle'><i>I</i></button>
-                    <button className='button-middle'><u>U</u></button>
-                    <button className='button-right'><s>S</s></button>
+                    <div className='button-group'>
+                        <button className='button-left'><b>B</b></button>
+                        <button className='button-middle'><i>I</i></button>
+                        <button className='button-middle'><u>U</u></button>
+                        <button className='button-right'><s>S</s></button>
+                    </div>
+                    <div className='button-group'>
+                        <button className='button-left'>x<sup>2</sup></button>
+                        <button className='button-right'>x<sub>2</sub></button>
+                    </div>
+                    <div className='button-group'>
+                        <button className='button-left'><span className='material-symbols-outlined'>format_color_text</span></button>
+                        <button className='button-middle'><span className='material-symbols-outlined'>ink_highlighter</span></button>
+                        <button className='button-right'><span className='material-symbols-outlined'>format_clear</span></button>
+                    </div>
+                    <div className='button-group'>
+                        <button className='button-left'><span className='material-symbols-outlined'>format_list_numbered</span></button>
+                        <button className='button-middle'><span className='material-symbols-outlined'>format_list_bulleted</span></button>
+                        <button className='button-right'><span className='material-symbols-outlined'>format_align_center</span></button>
+                    </div>
+                    <div className='button-group'>
+                        <button className='button-left'><span className='material-symbols-outlined'>image</span></button>
+                        <button className='button-middle'><span className='material-symbols-outlined'>mic</span></button>
+                        <button className='button-right'><span className='material-symbols-outlined'>function</span></button>
+                    </div>
                 </div>
-                <div className='button-group'>
-                    <button className='button-left'>x<sup>2</sup></button>
-                    <button className='button-right'>x<sub>2</sub></button>
+                <div className='preview-section'>
+                    <FieldInput html={''} onChange={function (v: string): void {
+                        throw new Error('Function not implemented.');
+                    }} fieldName={'Question'}></FieldInput>
+                    <FieldInput html={''} onChange={function (v: string): void {
+                        throw new Error('Function not implemented.');
+                    }} fieldName={'Answer'}></FieldInput>
+                    <FieldInput html={''} onChange={function (v: string): void {
+                        throw new Error('Function not implemented.');
+                    }} fieldName={'Hint'}></FieldInput>
                 </div>
-                <div className='button-group'>
-                    <button className='button-left'><span className='material-symbols-outlined'>format_color_text</span></button>
-                    <button className='button-middle'><span className='material-symbols-outlined'>ink_highlighter</span></button>
-                    <button className='button-right'><span className='material-symbols-outlined'>format_clear</span></button>
-                </div>
-                <div className='button-group'>
-                    <button className='button-left'><span className='material-symbols-outlined'>format_list_numbered</span></button>
-                    <button className='button-middle'><span className='material-symbols-outlined'>format_list_bulleted</span></button>
-                    <button className='button-right'><span className='material-symbols-outlined'>format_align_center</span></button>
-                </div>
-                <div className='button-group'>
-                    <button className='button-left'><span className='material-symbols-outlined'>image</span></button>
-                    <button className='button-middle'><span className='material-symbols-outlined'>mic</span></button>
-                    <button className='button-right'><span className='material-symbols-outlined'>function</span></button>
-                </div>
-            </div>
-            </div>
-            <div className='preview-section'>
-            <FieldInput html={''} onChange={function (v: string): void {
-                throw new Error('Function not implemented.');
-            } } fieldName={'Question'}></FieldInput>
-            <FieldInput html={''} onChange={function (v: string): void {
-                throw new Error('Function not implemented.');
-            } } fieldName={'Answer'}></FieldInput>
-            <FieldInput html={''} onChange={function (v: string): void {
-                throw new Error('Function not implemented.');
-            } } fieldName={'Hint'}></FieldInput>
             </div>
         </div>
     );

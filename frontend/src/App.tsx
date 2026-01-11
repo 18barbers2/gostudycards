b7267dd13a6c644b09653472e3276670c68ac331
18barbers2 <sambarber101@gmail.com>
2026-01-11 11:35:41 -0500

    feature: added two pages to the app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import NavBar from './components/NavBar.tsx';
+import CardBuilder from './pages/CardBuilder.tsx';
+import AddCard from './pages/AddCard.tsx';
+
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>Studying done well</h1>
-      </div>
-    </>
+    <div className="app">
+      <NavBar/>
+
+      <Routes>
+        <Route path="/"/>
+        <Route path="/add-card" element={<AddCard/>}/>
+        <Route path="/card-builder" element={<CardBuilder/>}/>
+      </Routes>
+
+    </div>
   );
 }
 

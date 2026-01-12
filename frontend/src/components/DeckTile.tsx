dfc86369e3cc768b06a92d26d9a15e12ea110592
18barbers2 <sambarber101@gmail.com>
2026-01-11 19:35:22 -0500

    feature: added the Deck Tile component

---
+++ frontend/src/components/DeckTile.tsx
@@ -0,0 +1,10 @@
+export function DeckTile() {
+    return (
+        <div className='deck-tile'>
+            <h3>Deck Title</h3>
+            <p>Number of Cards: 42</p>
+        </div>
+    );
+}
+
+export default DeckTile;
--- frontend/src/pages/Decks.tsx
+++ frontend/src/pages/Decks.tsx
@@ -1,11 +1,12 @@
 import '../css/Decks.css';
+import DeckTile from '../components/DeckTile.tsx';
 
 export function Decks() {
     return (
         <div className='decks-page'>
             <h2>Decks</h2>
             <p>This is the Decks page. Here you will be able to view and manage your decks of cards.</p>
-            
+            <DeckTile/>
         </div>
     );
 }

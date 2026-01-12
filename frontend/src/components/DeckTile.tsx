b10ef276b0fdc8bc35dc5ec7494bd71f744305d6
18barbers2 <sambarber101@gmail.com>
2026-01-11 20:07:40 -0500

    feature: updated the props for the DeckTile

--- frontend/src/components/DeckTile.tsx
+++ frontend/src/components/DeckTile.tsx
@@ -1,13 +1,17 @@
 import '../css/DeckTile.css';
 
 
-export function DeckTile() {
+export function DeckTile({ title, createdBy, deckId, onMenuClick }: { title?: string; createdBy?: string; deckId?: string; onMenuClick?: (deckId: string) => void }) {
     return (
         <div className='deck-tile'>
-            <h3>Deck Title</h3>
-            <p>Number of Cards: 42</p>
+            <button className='deck-menu' onClick={() => onMenuClick && deckId && onMenuClick(deckId)}>⋯</button>
+            <div className='deck-content'>
+                <h3 className='deck-title'>Deck Title</h3>
+                {/* <p>Number of Cards: 42</p> */}
+                <p className='created-by'>created by: samuel barber</p>
+            </div>
         </div>
     );
 }
 
-export default DeckTile;
+export default DeckTile;  

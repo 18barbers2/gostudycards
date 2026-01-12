2178901ec9fc270dbee710f633e27ed87eda508e
18barbers2 <sambarber101@gmail.com>
2026-01-11 21:32:19 -0500

    fix: fixed issue with the DeckTileProps being declared twice

--- frontend/src/components/DeckTile.tsx
+++ frontend/src/components/DeckTile.tsx
@@ -1,13 +1,26 @@
 import '../css/DeckTile.css';
 
+interface DeckTileProps {
+    title?: string;
+    createdBy?: string;
+    deckId?: string;
+    stats?: {
+        cards: number;
+        needReview: number;
+        new: number;
+        learned: number;
+        lastReviewed: string;
+    };
+    onMenuClick?: (deckId: string) => void;
+}
 
-export function DeckTile({ title, createdBy, deckId, onMenuClick }: { title?: string; createdBy?: string; deckId?: string; onMenuClick?: (deckId: string) => void }) {
+export function DeckTile({ title, createdBy, deckId, onMenuClick }: DeckTileProps){
     return (
         <div className='deck-tile'>
             <button className='deck-menu' onClick={() => onMenuClick && deckId && onMenuClick(deckId)}>⋯</button>
             <div className='deck-content'>
-                <h3 className='deck-title'>Deck Title</h3>
-                {/* <p>Number of Cards: 42</p> */}
+                <h3 className='deck-title'>{ title }</h3>
+                <p>Number of Cards: 42</p>
                 <p className='created-by'>created by: samuel barber</p>
             </div>
         </div>

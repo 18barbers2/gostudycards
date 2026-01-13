30ad6f97a002b3b89b3ac4d423c1455692c87076
18barbers2 <sambarber101@gmail.com>
2026-01-12 20:24:37 -0500

    feature: added the homepage basic layout

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import '../css/Home.css';
+
+export function Home() {
+    return (
+        <div className='home-page'>
+            <h1>Welcome to GoStudyCards!</h1>
+            <p>This is the home page. Use the navigation bar to explore the app.</p>
+        </div>
+    );
+}
+
+export default Home;

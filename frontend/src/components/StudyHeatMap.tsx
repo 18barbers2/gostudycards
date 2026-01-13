3c38b4b79934eecdf839182510abc8145db7536a
18barbers2 <sambarber101@gmail.com>
2026-01-12 21:52:57 -0500

    uninstall: removed react-heatmap-grid

---
+++ frontend/src/components/StudyHeatMap.tsx
@@ -0,0 +1,25 @@
+import HeatMap from 'react-heatmap-grid';
+
+export function StudyHeatMap() {
+    const xLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const yLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const data = 
+    [
+        [1, 2, 3, 4, 5, 6, 7],
+        [2, 3, 4, 5, 6, 7, 8],
+    ];
+
+
+    return (
+        <div className='heatmap'>
+            <h2>Study Heatmap</h2>
+            <p>Heatmap visualization goes here.</p>
+            <HeatMap
+                xLabels={xLabels}
+                yLabels={yLabels}
+                data={data}></HeatMap>
+        </div>
+    );
+}
+
+export default StudyHeatMap;

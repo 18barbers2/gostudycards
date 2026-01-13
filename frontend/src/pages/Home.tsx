aa53279a7836dc3ce2e2c196c38f8d22c4545898
18barbers2 <sambarber101@gmail.com>
2026-01-12 21:38:34 -0500

    feature: added the layout for the homepage cards

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,17 @@ export function Home() {
         <div className='home-page'>
             <h1>Welcome to GoStudyCards!</h1>
             <p>This is the home page. Use the navigation bar to explore the app.</p>
+            <div className='home-page-container'>
+                <div className='welcome-section'>Hi, Sam</div>
+                <div className='cards-due-section'> 42 Cards Due</div>
+                <div className='card-piechart-section'>Pie Chart Goes Here</div>
+                <div className='heatmap-section'>Heatmap goes here</div>
+                <div className='quick-actions-section'>
+                    <button>Study Now</button>
+                    <button>Add New Card</button>
+                </div>
+            </div>
+            
         </div>
     );
 }

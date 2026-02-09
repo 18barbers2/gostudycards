3872d91aadd79fe13266187acfa9d6b438455af0
18barbers2 <sambarber101@gmail.com>
2026-02-08 19:17:47 -0500

    feature: replaced welcome section with new component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import '../css/Home.css';
+import { DashboardCard, GreetingCardContent} from '../components/DashboardCard';
 
 export function Home() {
     return (
@@ -6,7 +7,9 @@ export function Home() {
             <h1>Welcome to GoStudyCards!</h1>
             <p>This is the home page. Use the navigation bar to explore the app.</p>
             <div className='home-page-container'>
-                <div className='welcome-section'>Hi, Sam</div>
+                <DashboardCard title='Welcome back!' colorGradient={['#ff7e5f', '#feb47b']}>
+                    <GreetingCardContent userName='Sam' deckCount={5} cardCount={120} />
+                </DashboardCard>
                 <div className='cards-due-section'> 42 Cards Due</div>
                 <div className='card-piechart-section'>Pie Chart Goes Here</div>
                 <div className='heatmap-section'>Heatmap goes here</div>

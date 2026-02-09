7583c925354992e347c2379e75b36df5a182cb6c
18barbers2 <sambarber101@gmail.com>
2026-02-09 00:50:56 -0500

    style: fixed centering issue with study page

--- frontend/src/pages/Study.tsx
+++ frontend/src/pages/Study.tsx
@@ -1,10 +1,20 @@
 import { Layout } from "../components/Layout/Layout.tsx";
+import Card from "../components/Card.tsx";
+
 
 export function Study() {
     return (
         <Layout>
-            <div className="study">
+            <div className='study-page'>  
                 <h1>Study</h1>
+                <div className="study-page-container">
+                    <Card
+                        id="1"
+                        deckId="1"
+                        template="<div>{{front}}</div>"
+                        data={{ front: "What is the capital of France?" }}
+                    />
+                </div>
             </div>
         </Layout>
     );

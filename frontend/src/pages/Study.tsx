0e7c65d65bb44ec95d848719e8f9e2ffd46297f0
18barbers2 <sambarber101@gmail.com>
2026-02-09 21:33:59 -0500

    feature: added deck options with styling

--- frontend/src/pages/Study.tsx
+++ frontend/src/pages/Study.tsx
@@ -1,16 +1,40 @@
 import { Layout } from "../components/Layout/Layout.tsx";
+import testDecks from '../data/testDecks.json';
+import '../css/Study.css';
+
+
+export function DeckOption( {id, name, description, cardsDue} : {id: string, name: string, description: string, cardsDue: number} ) {
+    return (
+        <div className="deck-option">
+            <div className="deck-info">
+                <h3>{name}</h3>
+                <p>{description}</p>
+            </div>
+            <div className="deck-stats">
+                <div className="due-count">{cardsDue}</div>
+                <div className="due-label">cards due</div>
+            </div>
+        </div>
+    );
+}
 
 
 export function Study() {
     return (
         <Layout>
-            <div className='study-page'>  
-                <h1>Study</h1>
-                <h2 className='subtitle'>Choose a deck to study</h2>
-                <div className="study-page-container">
-
-                </div>
+            <h1>Study</h1>
+            <h2 className='subtitle'>Choose a deck to study</h2>
+            <div className="deck-selection">
+                {testDecks.map((deck) => (
+                    <DeckOption
+                        id={deck.id}
+                        name={deck.name}
+                        description={deck.description || "No description"}
+                        cardsDue={deck.cardsDue}
+                    />
+                ))}
             </div>
+       
         </Layout>
     );
 }

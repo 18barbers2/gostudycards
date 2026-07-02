149b8ca68ec606299d76b5f9140f896d569c445d
Samuel Barber <47995052+18barbers2@users.noreply.github.com>
2026-07-02 19:58:50 -0400

    fix: regex now replaces all variables with values. on card

--- frontend/src/utils/templateParser.tsx
+++ frontend/src/utils/templateParser.tsx
@@ -2,7 +2,7 @@ export function parseTemplate ( template : string, data : any): string {
     let result = template;
 
     for (let key in data) {
-        result = result.replace(`{{${key}}}`, data[key]);
+        result = result.replaceAll(`{{${key}}}`, data[key]);
     }
 
     return result;

44d8ed44349156a943944cfd0ad68bd84dfd1686
Samuel Barber <47995052+18barbers2@users.noreply.github.com>
2026-07-02 20:05:43 -0400

    feat: removed redundant buttons from format controls

--- frontend/src/components/EditorFormatControls.tsx
+++ frontend/src/components/EditorFormatControls.tsx
@@ -9,21 +9,19 @@ export function EditorFormatControls({ handleFormat, disabled = false }: { handl
                 <button className={btn('button-left')} disabled={disabled} onClick={() => handleFormat('bold')}><b>B</b></button>
                 <button className={btn('button-middle')} disabled={disabled} onClick={() => handleFormat('italic')}><i>I</i></button>
                 <button className={btn('button-middle')} disabled={disabled} onClick={() => handleFormat('underline')}><u>U</u></button>
-                <button className={btn('button-right')} disabled={disabled} onClick={() => handleFormat('strikethrough')}><s>S</s></button>
+                <button className={btn('button-middle')} disabled={disabled} onClick={() => handleFormat('strikethrough')}><s>S</s></button>
+                <button className={btn('button-right')} disabled={disabled} onClick={() => handleFormat('highlight')}><span className='material-symbols-outlined'>ink_highlighter</span></button>
+
             </div>
             <div className='button-group'>
                 <button className={btn('button-left')} disabled={disabled} onClick={() => handleFormat('superscript')}>x<sup>2</sup></button>
                 <button className={btn('button-right')} disabled={disabled} onClick={() => handleFormat('subscript')}>x<sub>2</sub></button>
             </div>
             <div className='button-group'>
-                <button className={btn('button-left')} disabled={disabled}><span className='material-symbols-outlined'>format_color_text</span></button>
-                <button className={btn('button-middle')} disabled={disabled} onClick={() => handleFormat('highlight')}><span className='material-symbols-outlined'>ink_highlighter</span></button>
-                <button className={btn('button-right')} disabled={disabled} onClick={() => handleFormat('clear')}><span className='material-symbols-outlined'>format_clear</span></button>
             </div>
             <div className='button-group'>
                 <button className={btn('button-left')} disabled={disabled} onClick={() => handleFormat('ordered_list')}><span className='material-symbols-outlined'>format_list_numbered</span></button>
-                <button className={btn('button-middle')} disabled={disabled} onClick={() => handleFormat('unordered_list')}><span className='material-symbols-outlined'>format_list_bulleted</span></button>
-                <button className={btn('button-right')} disabled={disabled} onClick={() => handleFormat('center_align')}><span className='material-symbols-outlined'>format_align_center</span></button>
+                <button className={btn('button-right')} disabled={disabled} onClick={() => handleFormat('unordered_list')}><span className='material-symbols-outlined'>format_list_bulleted</span></button>
             </div>
         </div>
     );
